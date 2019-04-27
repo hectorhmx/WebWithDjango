65916767bb84e13dee3a22dc58f06c36ea749702
hectorhmx <hectorhmx.55@gmail.com>
2019-04-27 14:01:16 -0500

    Added a model

--- mi_sitio/apps/blog/models.py
+++ mi_sitio/apps/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Publication(models.Model):
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    fecha = models.DateTimeField(auto_now_add=True)
+    ##En caso de que borre un usuario, se borrarán todas sus publicaciones
+    autor = models.ForeignKey(User,on_delete=models.CASCADE)
+    #Imagen se guardará por año, mes y día, y puede estar vació en la base y en nuestro archivos
+    imagen = models.ImageField(upload_to='imagenes/publicaciones/%Y%m%d/',blank=True,null=True)
+    def __str__(self):
+        return f'{self.autor} {self.fecha}'
+    
--- mi_sitio/apps/blog/views.py
+++ mi_sitio/apps/blog/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Publication
 # Create your views here.
 
 def home(request):
 	return render(request,'blog/index.html',{})
 
 def verposts(request):
-	return render(request,'blog/posts.html',{})
+	publicaciones = Publication.objects.all()##Traigo todas las publicaciones
+	##Y las almaceno a este lugar
+	return render(request,'blog/posts.html',{'publicaciones':publicaciones})
